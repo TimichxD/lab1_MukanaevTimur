d13bf56f2a3b544353d9195c6401b6b9ad612fb4
liltimi4 <mukanaevtimur07@gmail.com>
2026-09-25 16:37:10 +0500

    второй коммит: добавлены следующие принты: группу и подгруппу, текущую дату и текст песни

--- lab1_1_MukanaevTimur.py
+++ lab1_1_MukanaevTimur.py
@@ -1,4 +1,7 @@
 def main():
-    print('My first program')
+    print('Муканаев Тимур')
+    print('ИВТ-ИВС-203Б')
+    print('25.09.26')
+    print('общепит каждый день одно и то же')
 if __name__ == '__main__':
     main()
